e3efba9eed6a1e29458154b7f15a73b2a8921283
AntonSik <121441647+AntonSik@users.noreply.github.com>
2023-11-22 11:08:29 +0300

    Delete app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffInterface.java

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffInterface.java
+++
@@ -1,5 +0,0 @@
-package ru.yandex.practicum.contacts.presentation.base;
-
-public interface BaseListDiffInterface<T extends ListDiffInterface<BaseListDiffCallback>> {
-    boolean areItemsTheSame(T t);
-}
